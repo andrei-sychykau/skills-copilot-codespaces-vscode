13d8b3800f8b61c29040c8f968c3b524ebb6860f
andrei-sychykau <andrei_sychykau@epam.com>
2025-03-06 06:55:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create web server
+var express = require('express');
+var app = express();
 
-const comments = require('./comments.json');
-
-app.get('/comments', (req, res) => {
-  res.send(comments);
+// Create a route for the home page
+app.get('/', function(req, res) {
+  res.send('Welcome to the home page!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+// Create a route for comments page
+app.get('/comments', function(req, res) {
+  res.send('Welcome to the comments page!');
+});
+
+// Start the server on port 8080
+app.listen(8080, function() {
+  console.log('Server running on http://localhost:8080');
 });
